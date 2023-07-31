6c4086f6eb0cffc2d0939d44045e9b27fa84ff85
czjge <1159617259@qq.com>
2023-07-31 11:28:04 +0800

    add gin

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello wj!")
+
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(ctx *gin.Context) {
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+
+		acceptString := ctx.GetHeader("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			ctx.String(http.StatusNotFound, "404 Not Found")
+		} else {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "Route not found",
+			})
+		}
+
+	})
+
+	r.Run(":8001")
 }
