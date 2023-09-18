c6676388b2d17eff08547facb11fe383a73a9794
czjge <1159617259@qq.com>
2023-09-18 11:08:41 +0800

    chore

---
+++ app/http/middlewares/guest_jwt.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"github.com/czjge/gohub/pkg/jwt"
+	"github.com/czjge/gohub/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+func GuestJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		if len(c.GetHeader("Authorization")) > 0 {
+
+			_, err := jwt.NewJWT().ParseToken(c)
+			if err != nil {
+				response.Unauthorized(c, "请使用游客身份访问")
+				c.Abort()
+				return
+			}
+		}
+
+		c.Next()
+	}
+}
