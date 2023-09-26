c9476dd0e739b04f27ff83f9bd4a70f581771723
czjge <1159617259@qq.com>
2023-09-26 14:25:54 +0800

    chore

--- routes/api.go
+++ routes/api.go
@@ -40,6 +40,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
 			tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
+			tpcGroup.DELETE("/:id", middlewares.AuthJWT(), tpc.Delete)
 		}
 
 		authGroup := v1.Group("/auth")
